6f7a4ded419a94d37607c702a69510edd4bcba4c
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-05 10:31:09 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,21 +1,27 @@
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+  return rand(1..11)
 end
 
-def display_card_total
+def display_card_total(card_total)
   # code #display_card_total here
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
   # code #prompt_user here
+  "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  user_input = gets.chomp
+  return user_input
 end
 
 def end_game
@@ -40,4 +46,13 @@ end
 
 def runner
   # code runner here
+  prompt_user
+  if user_input == 'h'
+    hit?
+  elsif user_input == 's'
+    stay?
+  elsif
+    invalid_command
+    prompt_user
+  end
 end
